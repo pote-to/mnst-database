8d8f9f83d8c8a82f0c6a7392151abcb1384522c1
atlas108xd@gmail.com <atlas108xd@gmail.com>
2024-07-23 19:57:28 +0900

    develop first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // site: 'https://pote-to.github.io',
+    // site: 'https://pote-to.github.io/mnst-database',
+    // base: '/mnst-database',
+});
